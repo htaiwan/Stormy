ae28a3c1987973845058eeb37db0b8d848ed9a2d
Mr. Null <htaiwan@gmail.com>
2015-06-14 19:43:51 +0800

    finish this project

--- Stormy/app/src/main/java/com/htaiwan/app/stormy/CurrentWeather.java
+++ Stormy/app/src/main/java/com/htaiwan/app/stormy/CurrentWeather.java
@@ -24,6 +24,43 @@ public class CurrentWeather {
         mIcon = icon;
     }
 
+    public int getIconId() {
+        int iconId = R.drawable.clear_day;
+
+        if (mIcon.equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        }
+        else if (mIcon.equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.drawable.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.drawable.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.drawable.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.drawable.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.drawable.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.drawable.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+
+        return iconId;
+    }
+
     public long getTime() {
         return mTime;
     }
@@ -32,11 +69,13 @@ public class CurrentWeather {
         mTime = time;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public int getTemperature() {
+
+        return (int)Math.round(mTemperature);
     }
 
     public void setTemperature(double temperature) {
+
         mTemperature = temperature;
     }
 
@@ -48,8 +87,9 @@ public class CurrentWeather {
         mHumidity = humidity;
     }
 
-    public double getPrecipChance() {
-        return mPrecipChance;
+    public int getPrecipChance() {
+        double percetage = mPrecipChance * 100;
+        return (int)Math.round(percetage);
     }
 
     public void setPrecipChance(double precipChance) {
--- Stormy/app/src/main/java/com/htaiwan/app/stormy/MainActivity.java
+++ Stormy/app/src/main/java/com/htaiwan/app/stormy/MainActivity.java
@@ -1,6 +1,7 @@
 package com.htaiwan.app.stormy;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
@@ -8,7 +9,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ImageView;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -21,23 +26,53 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     private CurrentWeather mCurrentWeather;
+    @InjectView(R.id.timeLabel) TextView  mTimeLabel;
+    @InjectView(R.id.temptureLabel) TextView mTemperatureLabel;
+    @InjectView(R.id.humdityValue) TextView mHumidityValue;
+    @InjectView(R.id.precipValue) TextView mPrecipValue;
+    @InjectView(R.id.summary) TextView mSummaryLabel;
+    @InjectView(R.id.iconimageView) ImageView mIconImageView;
+    @InjectView(R.id.refleshImageView) ImageView mRefreshImageView;
+    @InjectView(R.id.progressBar) ProgressBar mProgressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        final double latitude = 25.02; // 37.8267
+        final double longitude = 121.31;
+
+        mProgressBar.setVisibility(View.INVISIBLE);
+
+        mRefreshImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getForecast(latitude, longitude);
+            }
+        });
+
+        getForecast(latitude, longitude);
+    }
+
+    private void getForecast(double latitude, double longitude) {
         // https://api.forecast.io/forecast/f6d8566ede5fd9513e8b0cc891765144/37.8267,-122.423
         String APIKey = "f6d8566ede5fd9513e8b0cc891765144";
-        double latitude = 37.8267; // 37.8267
-        double longitude = -122.423;
         String forcastUrl = "https://api.forecast.io/forecast/" + APIKey +
                 "/" + latitude + "," + longitude;
         if (isNetworkAvailable()) {
+            toggleRefresh();
+
             OkHttpClient client = new OkHttpClient();
             final Request request = new Request.Builder()
                     .url(forcastUrl)
@@ -47,24 +82,41 @@ public class MainActivity extends ActionBarActivity {
             call.enqueue(new Callback() {
                 @Override
                 public void onFailure(Request request, IOException e) {
-
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toggleRefresh();
+                        }
+                    });
+                    alertUserAboutError();
                 }
 
                 @Override
                 public void onResponse(Response response) throws IOException {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toggleRefresh();
+                        }
+                    });
+
                     try {
                         String jsonData = response.body().string();
                         Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
                             mCurrentWeather = getCurrentDetails(jsonData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+                                }
+                            });
                         } else {
                             alertUserAboutError();
                         }
-                    }
-                    catch (IOException e) {
+                    } catch (IOException e) {
                         Log.e(TAG, "Exception Caught:", e);
-                    }
-                    catch (JSONException e) {
+                    } catch (JSONException e) {
                         Log.e(TAG, "Exception Caught:", e);
                     }
                 }
@@ -72,8 +124,29 @@ public class MainActivity extends ActionBarActivity {
         } else {
             Toast.makeText(this, getString(R.string.network_unavilable_message), Toast.LENGTH_LONG).show();
         }
+    }
 
+    private void toggleRefresh() {
+        if (mProgressBar.getVisibility() == View.INVISIBLE) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mRefreshImageView.setVisibility(View.INVISIBLE);
+        }
+        else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mRefreshImageView.setVisibility(View.VISIBLE);
+        }
 
+    }
+
+    private void updateDisplay() {
+        mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
+        mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + " it will be");
+        mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
+        mPrecipValue.setText(mCurrentWeather.getPrecipChance() + "%");
+        mSummaryLabel.setText(mCurrentWeather.getSummary());
+
+        Drawable drawable = getResources().getDrawable(mCurrentWeather.getIconId());
+        mIconImageView.setImageDrawable(drawable);
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException{
